27d1876c302be2e20ab64b068caee300bd8e0e78
[zebulon0] <zah1276@rit.edu>
2022-04-12 09:22:00 -0400

    first

---
+++ DFT - Template.cpp
@@ -0,0 +1,16 @@
+#include <complex>
+#include <iostream>
+#include <math.h>
+#include "DFT.h"
+
+/// <summary>
+///  DFT
+/// </summary>
+/// <param name="in"></param>
+/// <param name="N"></param> // number of elements
+/// <param name="out"></param>
+void DFT (double x[], int N, complex<double> X[])
+{
+
+}
+
---
+++ DFT - Template.h
@@ -0,0 +1,12 @@
+#ifndef _DFT_H_
+#define _DFT_H
+using namespace std;
+#include <vector>
+#include <complex>
+#ifndef M_PI
+#define M_PI 3.1415926
+#endif
+
+void DFT (double x[], int N, complex<double> X[]);
+
+#endif // _DFT_H_
