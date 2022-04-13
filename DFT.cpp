0dbaa3a5a28cabb5862b0106a22be32e2b99cf1f
zebulon0 <zah1276@rit.edu>
2022-04-12 21:06:34 -0400

    problem 4 done???

--- DFT.cpp
+++ DFT.cpp
@@ -6,11 +6,25 @@
 /// <summary>
 ///  DFT
 /// </summary>
-/// <param name="in"></param>
-/// <param name="N"></param> // number of elements
-/// <param name="out"></param>
-void DFT (double x[], int N, complex<double> X[])
-{
+/// <param name="in"></param>   // Input signal
+/// <param name="N"></param>    // number of elements
+/// <param name="out"></param>  // Output "transformed signal" in frequency domain
+void DFT (double x[], int N, complex<double> X[]){
+    complex<double> sum;
+    // For each output value X[k]
+    for( int k = 0; k < N; k++ ){
+
+        // Do the summation over all X[n] values
+        sum = 0;
+        for ( int n = 0; n < N; n++ ){
+            sum += x[n] * ( cos((2*M_PI*k*n)/N) - 1.0i * sin((2*M_PI*k*n)/N));
+        }
+        X[k] = sum;
+    }
+
 
 }
+
+
+
 
